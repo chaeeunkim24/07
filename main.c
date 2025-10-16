904e25319710780a803278f01ebcdd894d30f777
chaeeunkim24 <chae020703@sookmyung.ac.kr>
2025-10-16 15:36:10 +0900

    practice_3

--- main.c
+++ main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int inc(int counter);
+
 int main(void)
 {
-    int i;
-
-    for (i = 0; i < 5; i++)
-    {
-        int temp = 1;
-        printf("temp = %d\n", temp);
-        temp++;
-    }
+    int i = 10;
+    printf("함수 호출 전 i = %d\n", i);
+    i = inc(i); // 함수의 반환값을 이용하여 함수 밖에서도 변수 사용 가능 
+    printf("함수 호출 후 i = %d\n", i);
 
     system("PAUSE");
     return 0;
+}
+
+int inc(int counter)
+{
+    counter++;
+    return counter;
 }
